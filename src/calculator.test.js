d3dfd9bb6569bb18cb05a7b33896e336e36e93ce
Lucas Chin <lucaselliotchin@gmail.com>
2020-06-26 09:34:09 -0400

    more tests!

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,7 +3,7 @@ class Calculator {
     this.memory = []
   }
 
-  input(num) {
+  inputDigit(num) {
     this.memory.push(num)
   }
 }
@@ -19,13 +19,150 @@ describe('Calculator', () => {
     expect(sut.memory).toEqual([])
   })
 
-  describe('#input', () => {
-    it('saves number into memory', () => {
+  describe('#inputDigit', () => {
+    it('throws Error if arg is not a single digit', () => {
       const sut = new Calculator()
 
-      sut.input(3)
+      expect(() => sut.inputDigit(11)).toThrow()
+    })
 
-      expect(sut.memory).toEqual([3])
+    it('throws if arg is a negative digit', () => {
+      const sut = new Calculator()
+
+      expect(() => sut.inputDigit(-1)).toThrow()
+    })
+
+    it('registers and returns a single number', () => {
+      const sut = new Calculator()
+
+      const returnedValue = sut.inputDigit(3)
+
+      expect(returnedValue).toEqual('3')
+    })
+
+    it('returns a string of multiple consecutive numbers', () => {
+      const sut = new Calculator()
+
+      sut.inputDigit(1)
+      const resultOfTwoDigits = sut.inputDigit(2)
+      const resultOfThreeDigits = sut.inputDigit(3)
+      const resultOfFourDigits = sut.inputDigit(4)
+
+      expect(resultOfTwoDigits).toEqual('12')
+      expect(resultOfThreeDigits).toEqual('123')
+      expect(resultOfFourDigits).toEqual('1234')
+    })
+
+    describe('when an operator has been stored', () => {
+      it('returns a string of the equation', () => {
+        const sut = new Calculator()
+
+        sut.inputDigit(1)
+        sut.inputOp('+')
+        const result = sut.inputDigit(1)
+
+        expect(result).toEqual('1 + 1')
+      })
+
+
+    })
+  })
+
+  describe('#inputOp', () => {
+    describe('when there is a single digit in memory', () => {
+      it('returns a string of the partial equation', () => {
+        const sut = new Calculator()
+
+        sut.inputDigit(1)
+        const result = sut.inputOp('+')
+
+        expect(result).toEqual('1 +')
+      })
+    })
+
+    describe('when there are two digits and an operator in memory', () => {
+      it('returns calculated string with new input operator', () => {
+        const sut = new Calculator()
+
+        sut.inputDigit(1)
+        sut.inputOp('+')
+        sut.inputDigit(1)
+        const result = sut.inputOp('+')
+
+        expect(result).toEqual('2 +')
+      })
+    })
+
+    describe('operators', () => {
+      it('+', () => {
+        const sut = new Calculator()
+
+        sut.inputDigit(1)
+        sut.inputOp('+')
+        sut.inputDigit(1)
+        const result = sut.equals()
+
+        expect(result).toEqual('2')
+      })
+
+      it('-', () => {
+        const sut = new Calculator()
+
+        sut.inputDigit(1)
+        sut.inputOp('-')
+        sut.inputDigit(1)
+        const result = sut.equals()
+
+        expect(result).toEqual('0')
+      })
+
+      it('*', () => {
+        const sut = new Calculator()
+
+        sut.inputDigit(10)
+        sut.inputOp('*')
+        sut.inputDigit(10)
+        const result = sut.equals()
+
+        expect(result).toEqual('100')
+      })
+
+      it('/', () => {
+        const sut = new Calculator()
+
+        sut.inputDigit(10)
+        sut.inputOp('/')
+        sut.inputDigit(10)
+        const result = sut.equals()
+
+        expect(result).toEqual('1')
+      })
+    })
+
+    it('throws Error if there is no digit stored in memory', () => {
+      const sut = new Calculator()
+
+      expect(() => sut.inputOp('-')).not.toThrowError(TypeError)
+      expect(() => sut.inputOp('-')).toThrowError()
+    })
+  })
+
+  describe('#equals', () => {
+    it('calculates equation stored in memory', () => {
+      const sut = new Calculator()
+      sut.memory = [1, '+', 1]
+
+      const result = sut.equals()
+
+      expect(result).toEqual('2')
+    })
+
+    it('throws if the equation cannot be calculated', () => {
+      const sut = new Calculator()
+      sut.memory = [1, '+']
+
+      expect(() => sut.equals()).not.toThrowError(TypeError)
+      expect(() => sut.equals()).toThrowError()
     })
   })
 })
