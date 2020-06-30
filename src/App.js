bc3febbf55e841be33181c5fdd4a8b0f904618e0
W00ST <matthewortner@gmail.com>
2020-06-30 19:39:57 -0400

    connected inputDigit( )
    Co-authored-by: Luke Elliot <lucaselliotchin@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Calculator from './calculator.js'
+
+const calculator = new Calculator()
 
 function App() {
+  const [equation, setEquation] = useState(0)
+
+  const inputDigit = (event) => {
+    const innerText = event.target.innerText
+    const newDigit = parseInt(innerText, 10)
+    const outPut = calculator.inputDigit(newDigit)
+    setEquation(outPut)
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={'calcBorder'}>
+        <div className={'display'}>
+          <div>{equation}</div>
+        </div>
+        <div className={'buttons'}>
+          <div onClick={inputDigit}>7</div>
+          <div onClick={inputDigit}>8</div>
+          <div onClick={inputDigit}>9</div>
+          <div>/</div>
+          <div onClick={inputDigit}>4</div>
+          <div onClick={inputDigit}>5</div>
+          <div onClick={inputDigit}>6</div>
+          <div>*</div>
+          <div onClick={inputDigit}>1</div>
+          <div onClick={inputDigit}>2</div>
+          <div onClick={inputDigit}>3</div>
+          <div>-</div>
+          <div>C</div>
+          <div onClick={inputDigit}>0</div>
+          <div>=</div>
+          <div>+</div>
+        </div>
+      </div>
     </div>
   );
 }
