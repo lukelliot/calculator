edfa0ca7cccdb0162b72b85c891746e819d98164
W00ST <matthewortner@gmail.com>
2020-06-29 23:15:05 -0400

    Co-authored-by: Luke Elliot <lucaselliotchin@gmail.com>

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,16 +1,59 @@
+const OPERATIONS = {
+  '+': (num1, num2) => {
+    return num1 + num2
+  },
+  '-': (num1, num2) => {
+    return num1 - num2
+  },
+  '*': (num1, num2) => {
+    return num1 * num2
+  },
+  '/': (num1, num2) => {
+    return num1 / num2
+  }
+}
+const OPERATORS = Object.keys(OPERATIONS)
+
 class Calculator {
   constructor() {
-    this.memory = [0]
-  }
+    this.memory = []
+    }
 
   inputDigit(num) {
     if (num < 0 || num >= 10 ) {
       throw new Error('invalid input, input only 0-9') 
     }
-    const newNum = (this.memory[0] * 10) + num
-    this.memory[0] = newNum
+
+    const index = OPERATORS.includes(this.memory[1]) ? 2 : 0
+
+    const newNum = (this.memory[index] || 0) * 10 + num
+    this.memory[index] = newNum
     
-    return `${newNum}`
+    return this.memory.join(' ')
+  }
+
+  inputOp(operator) {
+    if (!OPERATORS.includes(operator)) {
+      throw new Error(`Invalid operator. Use ${OPERATORS}`)
+    }
+    if (typeof this.memory[0] !== 'number') {
+      throw new Error('Digit required before operator')
+    }
+    if (this.memory.length === 3) {
+      this.memory[0] = this.operate()
+    }
+    this.memory[1] = operator
+    return `${this.memory[0]} ${this.memory[1]}`
+  }
+
+  equals() {
+    const calculation = this.operate()
+    this.memory = [calculation]
+    return `${calculation}`
+  }
+
+  operate() {
+    return OPERATIONS[this.memory[1]](this.memory[0], this.memory[2])
   }
 }
 
@@ -88,6 +131,7 @@ describe('Calculator', () => {
   describe('#inputOp', () => {
     it('throws on any invalid operator input', () => {
       const sut = new Calculator()
+      sut.memory = [1]
 
       expect(() => sut.inputOp('invalid')).not.toThrowError(TypeError)
       expect(() => sut.inputOp('invalid')).toThrowError()
@@ -113,9 +157,9 @@ describe('Calculator', () => {
         const sut = new Calculator()
 
         sut.memory = [1, '+', 1]
-        const result = sut.inputOp('+')
+        const result = sut.inputOp('-')
 
-        expect(result).toEqual('2 +')
+        expect(result).toEqual('2 -')
       })
     })
 
